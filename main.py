6ba2bba188ed58d77bb6890f3e904336934741e8
michaelvdb1 <michaelvdb@bipartners.nl>
2022-09-27 20:18:04 +0200

    Added scheduler

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from extract.extract_data import extract_data
 import load.load_datasets as datasets
 import load.load_refreshhistory as history
+from schedule import every, repeat, run_pending
 
+@repeat(every(1).minute)
 def main():
-    for i in range(200):
-        # Extract the data
-        extract_data()
+    # Extract the data
+    extract_data()
 
-        # Load the data into the DB
-        datasets.load_data()
-        history.load_data()
+    # Load the data into the DB
+    datasets.load_data()
+    history.load_data()
 
+while True:
+    run_pending()
 
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
